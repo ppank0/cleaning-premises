b4da5c9080033d96dae95b279c262c1f8d227c54
Pankova <pankkoo89@gmail.com>
2023-11-16 20:24:40 +0300

    feat: create Home page

---
+++ cleaning-premises/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from './../components/Footer'
+import Header from './../components/Header'
+
+const Home = () => {
+    return ( 
+        <>
+                HOME
+            <Footer/>
+        </>
+     );
+}
+ 
+export default Home;
