05f369bb364d9df3a7ae29372ec72027f7d7f219
Pankova <pankkoo89@gmail.com>
2023-11-16 20:23:16 +0300

    feat: add Router

--- cleaning-premises/src/components/Header.jsx
+++ cleaning-premises/src/components/Header.jsx
@@ -1,30 +1,33 @@
-import { Route, Router, Routes } from 'react-router-dom';
-import './../styles/header.css'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import './../styles/header.css';
+import Home from './../pages/Home'
 
 const Header = () => {
     return ( 
         <>
+        
+            <Router>
             <div className="header-container">
                 <h1 className="header-title">Cleaning Premises</h1>
                 {/* <div > */}
-                <label for="navbar-toggle" className="burger-icon">&#9776;</label>
+                <label htmlFor="navbar-toggle" className="burger-icon">&#9776;</label>
                     <ul className="nav-list">
-                        <li><a href="/">Главная</a></li>
-                        <li><a href="/brigades">Бригады</a></li>
-                        <li><a href="/schedule">График работ</a></li>
-                        <li><a href="/rooms">Помещения</a></li>
+                    <li><Link to="/">Главная</Link></li>
+                    <li><Link to="/brigades">Бригады</Link></li>
+                    <li><Link to="/schedule">График работ</Link></li>
+                    <li><Link to="/rooms">Помещения</Link></li>
                     </ul>
                 {/* </div> */}
             </div>
 
-            {/* <Router>
                 <Routes>
-                    <Route exect path='/' element={}/>
-                    <Route exect path='/brigades' element={}/>
+                    <Route path="/" element={<Home />} />
+                    {/* <Route exect path='/brigades' element={}/>
                     <Route exect path='/schedule' element={}/>
-                    <Route exect path='/rooms' element={}/>
+                    <Route exect path='/rooms' element={}/> */}
                 </Routes>
-            </Router> */}
+            </Router>
         </>
         
      );
