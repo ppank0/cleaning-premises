6f2f89293ced8d998cc8406d32cb392fb481bb2a
Pankova <pankkoo89@gmail.com>
2023-11-18 22:42:01 +0300

    feat: add one more section

--- cleaning-premises/src/pages/Home.jsx
+++ cleaning-premises/src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import Footer from './../components/Footer'
 import Header from './../components/Header'
+import AboutUs from '../components/AboutUs';
+import './../styles/home.css';
 
 const Home = () => {
     return ( 
         <>
-                HOME
+             <AboutUs/>
+             <div className="features">
+                <h2>Преимущества</h2>
+                <ul>
+                    <li>Высокий стандарт чистоты и качественная уборка</li>
+                    <li>Персонализированный подход к каждому клиенту</li>
+                    <li>Опытная команда профессионалов</li>
+                    <li>Использование экологически чистых средств и технологий</li>
+                    <li>Гибкий график работы и конкурентные цены</li>
+                </ul>
+            </div>
             <Footer/>
         </>
      );
