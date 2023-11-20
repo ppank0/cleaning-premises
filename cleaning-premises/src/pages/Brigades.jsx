98b89259e866c2d0e5c55a94ea6e2e981638fa06
antik <ant1kow22@gamil.com>
2023-11-20 22:39:29 +0300

    fix: fetch data

--- cleaning-premises/src/pages/Brigades.jsx
+++ cleaning-premises/src/pages/Brigades.jsx
@@ -1,61 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import BrigadeItem from '../components/BrigadeItem';
 import Footer from '../components/Footer';
+import data from './brigadesData.json'
 
 
 const Brigades = () => {
-    const [brigades, setBrigades] = useState([]);
-    
-    async function fetchData() {
-      try {
-        const response = await fetch('/brigadesData.json');
-        if (!response.ok) {
-          throw new Error(response.statusText);
-        }
-        const jsonData = await response.json();
-        console.log('ЭТО ДАННЫЕ:', jsonData);
-        setBrigades(jsonData);
-      } catch (error) {
-        console.error('Failed to fetch data:', error);
-      }
-    }
+  const [brigades, setBrigades] = useState([]);
+
   useEffect(() => {
-      fetchData();
+    setBrigades(data)
   }, []);
-  
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const response = await fetch('./brigadesData.json');
-  //       if (!response.ok) {
-  //         throw new Error('Ошибка загрузки данных');
-  //       }
-  //       const data = await response.json();
-  //       setBrigades(data);
-  //     } catch (error) {
-  //       console.error('Произошла ошибка:', error);
-  //     }
-  //   };
 
-  //   fetchData();
-  // }, []);
-  
-    if (brigades.length === 0) {
-      return <div>Loading...</div>;
-    }
+  if (brigades.length === 0) {
+    return <div>Loading...</div>;
+  }
 
-  
-    return ( 
-        <div className="brigades_page-container">
-            {brigades.map(brigade => (
-            <ul><li>{brigade.name}</li></ul>
-            ))}
-            <p>ТУТ ЕСТЬ ТЕЛО</p> 
-        </div>
-
-        
-      
-     );
+  return (
+    <div className="brigades_page-container">
+      <ul>
+        {brigades.map(brigade => (
+          <li key={brigade.id}>{brigade.name}</li>
+        ))}
+      </ul>
+      <p>ТУТ ЕСТЬ ТЕЛО</p>
+    </div>
+  );
 }
- 
+
 export default Brigades;
+
