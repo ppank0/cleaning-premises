5d5553c2320cbdaf80984e46230bb65bfcf7eb3e
Pankova <pankkoo89@gmail.com>
2023-11-21 11:29:21 +0300

    feat: add BrigadeItem and styles for it

--- cleaning-premises/src/components/BrigadeItem.jsx
+++ cleaning-premises/src/components/BrigadeItem.jsx
@@ -1,18 +1,38 @@
-import React from "react";
-import './../styles/brigade.css'
+import React, { useState } from "react";
+import './../styles/brigadeItem.css'
 
 const BrigadeItem = ({brigade}) => {
+    const [selectedMember, setSelectedMember] = useState(null);
+
+    const handleDoubleClick = (member) => {
+    setSelectedMember(member);
+  };
+  const handleCloseModal = () => {
+    setSelectedMember(null);
+  };
     return ( 
         <div className="brigade-container">
-            <h2 className="brigade-name">{brigade.name}</h2>
-            <ul>
+            <div className="brigadeItem-title">
+                <h2 className="brigade-name">{brigade.name}</h2>
+            </div>
+            <ul className="brigadeItem-member-container">
                 {brigade.members.map(member => (
-                    <li key={member.id}>
-                        <h3>{member.name}</h3>
+                    <li key={member.id} onDoubleClick={() => handleDoubleClick(member)}>
+                        <h3 className="brigadeItem_member-name">{member.name}</h3>
                         <p>Стаж работы: {member.experience}</p>
                     </li>
                 ))}
             </ul>
+
+            {selectedMember && (
+    <div className="modal-overlay">
+      <div className="modal">
+        <h3>{selectedMember.name}</h3>
+        <p>Стаж работы: {selectedMember.experience}</p>
+        <button onClick={handleCloseModal}>Закрыть</button>
+      </div>
+    </div>
+  )}
         </div>
      );
 }
