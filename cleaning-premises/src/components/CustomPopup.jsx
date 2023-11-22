48bbf872df3853b3883b57d6e055edddea4f72ac
Pankova <pankkoo89@gmail.com>
2023-11-22 22:53:56 +0300

    fix: change params

--- cleaning-premises/src/components/CustomPopup.jsx
+++ cleaning-premises/src/components/CustomPopup.jsx
@@ -2,10 +2,10 @@ import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
 import './../styles/customPopup.css'
 
-const CustomPopup = ({buttonText, popupContent}) => {
+const CustomPopup = ({item, popupContent}) => {
 
     return ( 
-        <Popup trigger={<button className='popup-btn'>{buttonText}</button>} modal>
+        <Popup trigger={item} modal>
       {close => (
         <div  onClick={close}>
            <div onClick={e => e.stopPropagation()}>
