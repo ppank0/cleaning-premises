d35a9492117b03a3d999becc47cdb0560fd3b009
Pankova <pankkoo89@gmail.com>
2023-11-30 00:04:01 +0300

    feat: add card element

--- cleaning-premises/src/components/RoomItem.jsx
+++ cleaning-premises/src/components/RoomItem.jsx
@@ -5,17 +5,24 @@ import MyPopup from './CustomPopup'
 const RoomItem = ({room}) => {
     return ( 
         <>
-            <div className="roomItem-container">
-                <h3 className='roomItem_container-title'>{room.name}</h3>
+        
+        <div className="card" style={{width: '18rem'}}>
+        <div className="roomItem-card_header">
+                <h5 className="card-title">{room.name}</h5>
+        </div>
+            <hr className='roomItem-hr'/>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+               <div className="roomItem-popup_container">
                 <MyPopup 
                     item ={<button className='popup-btn'>Информация</button>}
                     popupContent={<div style={{padding:'2rem 4rem'}}>
                     <p style={{marginBottom:'1rem'}}>Статус: {room.cleaned}</p>
                     <p>Последняя уборка помещения была: {room.lastCleanedDate}</p>
-                </div>}
-                    />
-                
-            </div>
+                    </div>}
+                />
+               </div>
+        </div>
+            
         </>
      );
 }
